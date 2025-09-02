e5b4cbfe8a27dc337b4284d76c7684b2626260e6
Volodimir <volodimirbryatko@gmail.com>
2025-09-02 20:31:00 +0300

    solution4

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export const App = () => {
 
   const handleSelectGood = good => {
     if (selectedGood === good) {
-      setSelectedGood(null);
+      setSelectedGood("");
     } else {
       setSelectedGood(good);
     }
@@ -35,7 +35,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelectedGood(null)}
+            onClick={() => setSelectedGood("")}
           />
         )}
       </h1>
